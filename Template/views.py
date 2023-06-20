363df8164f9ddbf5e4bd3b1b3e10ec83f683d1f6
te94d <te94da@gmail.com>
2023-06-20 19:05:59 +0900

    fix ihako

--- Template/views.py
+++ Template/views.py
@@ -3,11 +3,9 @@ import datetime
 from django.http import HttpRequest
 from django.shortcuts import render
 
-from . import forms, models
+from .. import forms, models
 
-def reserve_del(request):
-    models.User.objects.filter(id=reserve_id).delete()
-    return menberinfo(request)
+
 
 def register(request):
     register_form = forms.Register_Form()
@@ -18,5 +16,18 @@ def register_check(request):
     register_form = forms.Register_Form(request.POST)
     if register_form.is_valid():
         models.User.objects.create(
-            
+            name = register_form.cleaned_data['name'],
+            email = register_form.cleaned_data['email'],
+            password = register_form.cleaned_data['password'],
+            birth = register_form.cleaned_data['birth'],
+            address = register_form.cleaned_data['address'],
+            phone_num = register_form.cleaned_date['phone_num'],
         )
+        member = models.User.objects.get(
+            username=register_form.cleaned_data['username']
+        )
+        context = {'member': member}
+        return render(request, 'registercheck.html', context
+            )
+
+    return hukumitsu(request)
